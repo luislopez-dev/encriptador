44376d4ca18bed2e3ba5be49c1b31c6d9cab8eed
Luis Lopez <luis.dev.master@gmail.com>
2023-01-27 22:43:37 -0600

    Creating copy button

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function encrypt() {
 function decrypt(){
   out_box.value = out_box.value.split("").reverse().join("");
 }
+
+function copy_text(){
+
+  var copyText = in_box;
+
+  copyText.select();
+
+  copyText.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(copyText.value);
+  
+}
